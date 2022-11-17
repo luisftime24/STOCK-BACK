ed94197ec3149b5d11ae9ef29987575f1fa33ef4
luisftime24 <luisftime@gmail.com>
2022-11-17 00:30:08 -0500

    add new cors

--- stockAnalizer/settings.py
+++ stockAnalizer/settings.py
@@ -133,5 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:3000",
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "https://6375c692cacb11299f76b2fa--levistock.netlify.app/"
 ]
